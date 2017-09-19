ccae3b395c5459c1e4497ca955a2f4cbfe89e0ca
Somaya Rezai <somayarezai@Somayas-MacBook-Pro.local>
2017-09-19 10:51:59 +0200

    lab3 package documentation

---
+++ lab3/R/lab3.R
@@ -0,0 +1,11 @@
+#' Euclidean package.
+#'
+#' The Euclidean package provides one functions:
+#' euclidean.
+#' 
+#' @section Euclidean functions:
+#'  it returens the GCD
+#'
+#' @docType package
+#' @name lab3
+NULL
